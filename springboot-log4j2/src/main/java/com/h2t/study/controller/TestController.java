49074af868988c3bac7df95aa6b2c08e6627c058
shuangyueliao <1490612122@qq.com>
2019-12-26 01:47:17 +0800

    添加warn日志

--- springboot-log4j2/src/main/java/com/h2t/study/controller/TestController.java
+++ springboot-log4j2/src/main/java/com/h2t/study/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.h2t.study.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,24 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @version 1.0
  * @Date 2019/08/09 9:31
  */
-//@Log4j2
+@Slf4j
 @RestController
 public class TestController {
-    //Logger log = LogManager.getLogger(TestController.class);
-    Logger log = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/api/test")
     public Object hello() {
         log.trace("【TestController.class】trace level log input");
-        System.out.println("【TestController.class】trace level log input");
         log.debug("【TestController.class】debug level log input");
-        System.out.println("【TestController.class】debug level log input");
         log.info("【TestController.class】info level log input");
-        System.out.println("【TestController.class】info level log input");
         log.warn("【TestController.class】warn level log input");
-        System.out.println("【TestController.class】warn level log input");
         log.error("【TestController.class】error level log input");
-        System.out.println("【TestController.class】error level log input");
         return "hello world";
     }
 }
